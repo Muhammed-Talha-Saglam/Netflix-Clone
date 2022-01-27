6e0305083cb6b21cfbecdebb7181d69cfb56c8dc
muhammedtalhasaglam <talha.saglam@tokeninc.com>
2022-01-27 20:50:07 +0300

    Implemeted Upcoming Page UI.

--- Netflix Clone/controllers/UpcomingViewController.swift
+++ Netflix Clone/controllers/UpcomingViewController.swift
@@ -8,11 +8,69 @@
 import UIKit
 
 class UpcomingViewController: UIViewController {
+    
+    private var titles: [Title] = [Title]()
+    
+    private let upcomingTableView: UITableView = {
+       let tableView = UITableView()
+        tableView.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return tableView
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        title = "Upcoming"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(upcomingTableView)
+        upcomingTableView.delegate = self
+        upcomingTableView.dataSource = self
+        fetchUpcoming()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcomingTableView.frame = view.bounds
+    }
+    
+    private func fetchUpcoming() {
+        APICaller.shared.getUpcomingMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.upcomingTableView.reloadData()
+                }
+                print(titles)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {
+            return UITableViewCell()
+        }
+                
+        let title = titles[indexPath.row]
+        cell.configure(with: TitleViewModel(titleName: (title.original_name ?? title.original_title) ?? "Unknown Name", posterURL: title.poster_path ?? ""))
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
     }
     
 }
